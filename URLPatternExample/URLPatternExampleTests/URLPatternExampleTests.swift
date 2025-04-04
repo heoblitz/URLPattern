8b11e446fec3d3dbde6de83c268e9f2c635b0183
heoblitz <wone2287@gmail.com>
2025-04-04 18:51:16 +0900

    Remove host in URLPatternExampleTests

--- URLPatternExample/URLPatternExampleTests/URLPatternExampleTests.swift
+++ URLPatternExample/URLPatternExampleTests/URLPatternExampleTests.swift
@@ -6,8 +6,34 @@
 //
 
 import Testing
-import Foundation
-@testable import URLPatternExample
+import URLPattern
+
+@URLPattern
+enum DeepLinkMock: Equatable {
+  @URLPath("/home")
+  case home
+  
+  @URLPath("/posts/{postId}")
+  case post(postId: String)
+  
+  @URLPath("/posts/{postId}/comments/{commentId}")
+  case postComment(postId: String, commentId: String)
+  
+  @URLPath("/setting/{number}")
+  case setting(number: Int)
+  
+  @URLPath("/int/{int}")
+  case int(int: Int)
+  
+  @URLPath("/float/{float}")
+  case float(float: Float)
+  
+  @URLPath("/string/{string}")
+  case string(string: String)
+  
+  @URLPath("/c/{cNum}/b/{bNum}/a/{aNum}")
+  case complex(aNum: Int, bNum: Int, cNum: Int)
+}
 
 struct URLPatternExampleTests {
   // MARK: - Home Tests
